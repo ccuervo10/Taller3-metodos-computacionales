7b4395f8ba30f279c594c8720cd38790fc14241a
ccuervo10 <42391108+ccuervo10@users.noreply.github.com>
2018-10-31 17:47:17 -0500

    Transformada a columnas

--- Fourier.py
+++ Fourier.py
@@ -46,7 +46,7 @@ fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.plot(X[:,0],Y[:,0])
 ax.set_xlabel('x')
 ax.set_xlabel('y')
-fig.savefig(filename='ApellidoNombre_signal.pdf', type='pdf', dpi=100)
+fig.savefig(filename='CuervoCamilo_signal.pdf', type='pdf', dpi=100)
 
 def fourier(Y, ncolumnas):
     # Hacer transformada de cada columna en Y
@@ -97,5 +97,40 @@ def filtrar(X, Y, Xcorte, ncolumnas):
         Filtro[k,elegir_frecuencias] = Y[k,elegir_frecuencias]
     
     return Filtro
+
+def fourier(Y, ncolumnas):
+    # Hacer transformada de cada columna en Y
+    
+    Transformadas = np.zeros(np.shape(Y), dtype=complex)
+    
+    nfilas = np.size(Transformadas,0) # numero filas
+    
+    for k in range(nfilas):
+        for n in range(nfilas):
+            for i in range(ncolumnas):
+                # Hacer la trasnformada de todas las columnas al tiempo
+                Transformadas[k,i] = Transformadas[k,i] + Y[n,i]*np.exp(-1j*2*np.pi*n*k/nfilas)
+
+    return Transformadas
+
+
+
+def frecuencias(X, ncolumnas):
+    # Encontrar vector de frecuencias para cada columna
+    dx = X[1,:]-X[0,:] # delta de x
+    fs = 1/dx # frecuencias de muestreo
+    
+    Frecuencias = np.zeros(np.shape(X), dtype=float)
+    nfilas = np.size(Frecuencias, 0) # numero de filas
+    
+    for i in range(ncolumnas):
+        # crear vector de frecuencias para cada columna
+        Frecuencias[:,i] = np.linspace(-fs[i]/2, fs[i]/2, nfilas)
+    
+    # Rodar el arreglo de frecuencias para que coincidan con la transformada
+    Frecuencias = np.roll(Frecuencias, int(nfilas/2), axis=0)
+    
+    return Frecuencias
+
 
 

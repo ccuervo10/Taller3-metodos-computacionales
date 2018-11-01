e33dd3c08d746cec717e64ba7e84c8ba6c766ab3
ccuervo10 <42391108+ccuervo10@users.noreply.github.com>
2018-10-31 21:30:34 -0500

    filtro y graficas

--- Fourier.py
+++ Fourier.py
@@ -132,5 +132,78 @@ def frecuencias(X, ncolumnas):
     
     return Frecuencias
 
+def filtrar(X, Y, Xcorte, ncolumnas):
+    # Filtrar ncolumnas de Y acorde a valores de X
+    
+    # Iniciar filtro en ceros
+    Filtro = np.zeros(np.shape(Y), dtype=complex)
+    nfilas = np.size(Filtro, 0) # numero de filas
+    
+    # Encontrar los valores que se permiten pasar y replicarlos en Filtro
+    for k in range(nfilas):
+        elegir_frecuencias = np.abs(X[k,:]) < np.abs(Xcorte)
+        Filtro[k,elegir_frecuencias] = Y[k,elegir_frecuencias]
+    
+    return Filtro
+
+
+# Hacer la transformada de todas las senales
+T = fourier(Y, 3)
+
+# Encontrar las frecuencias de todas las senales
+F = frecuencias(X,3)
+
+# Filtrar con 1000 y con 500 como limite
+T1000 = filtrar(F, T, 1000, 3)
+T500 = filtrar(F, T, 500, 3)
+
+# Hacer transformadas inversas
+Y1000 = ifft(T1000, axis=0)
+Y500 = ifft(T500, axis=0)
+
+
+
+# Grafica de transformada de la senal
+fig, ax = plt.subplots(nrows=1, ncols=1)
+ax.plot(F[:,0],np.abs(T[:,0]))
+ax.set_xlabel('frecuencias')
+ax.set_xlim([-500,500])
+ax.grid()
+fig.savefig(filename='CuervoCamilo_TF.pdf', type='pdf', dpi=100)
+
+# Grafica de transformada inversa de la senal despues de filtrar a 1000Hz
+fig.clear()
+ax = fig.gca()
+ax.plot(X[:,0],np.real(Y1000[:,0]))
+ax.set_xlabel('x')
+fig.savefig(filename='CuervoCamilo_filtrada.pdf', type='pdf', dpi=100)
+
+# Grafica de espectros de frecuencia de las 3 senales
+fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(6,8))
+ax[0].plot(F[:,0],np.abs(T[:,0]))
+ax[0].set_title('original')
+ax[1].plot(F[:,1],np.abs(T[:,1]))
+ax[1].set_title('Interpolacion cuadratica')
+ax[2].plot(F[:,2],np.abs(T[:,2]))
+ax[2].set_title('Interpolacion cubica')
+ax[2].set_xlabel('frecuencias')
+fig.savefig(filename='CuervoCamilo_filtrada.pdf', type='pdf', dpi=100)
+
+# Grafica de transformada despues de filtrar a 1000Hz y a 500Hz
+fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(6,8))
+ax[0].plot(X,np.real(Y500))
+ax[0].set_title('Filtro 500Hz')
+ax[0].legend('123')
+ax[1].plot(X,np.real(Y1000))
+ax[1].set_title('Filtro 1000Hz')
+ax[1].set_xlabel('frecuencias')
+ax[1].legend('123')
+fig.savefig(filename='CuervoCamilo_2Filtros.pdf', type='pdf', dpi=100)
+
+print("Las frecuencias las calcule con mi propio codigo", "\n"*2)
+print("Los picos de frecuencia mas importantes estan en -170 y 170 Hz, segudidos por picos en -200 y 200Hz y -400 y 400Hz", "\n"*2)
+print("Los datos incompletos no tienen nigun patron en el periodo de muestreo, en general todos son diferentes. Por esa razon no tiene sentido calcular los multiplos de frecuencia 2pi*n*k de la formula de la transformada discreta", "\n"*2)
+print("A medida que aumenta el orden de la interpolacion los picos de 400Hz presentan magnitudes menores en el espectro de frecuencias. En la interpolacion cubica se activa ruido entre 500Hz y 2000 Hz. En la interpolacion cuadrada se activa ruido entre 500Hz y 5000Hz", "\n"*2)
+
 
 
